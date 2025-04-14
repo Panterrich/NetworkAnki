2cb3d846e19063690223e676b7b7ed0cdac651ca
panterrich <durnov.an@phystech.edu>
2025-04-14 15:36:15 +0300

    fix: pylint warnings

--- network_anki/views.py
+++ network_anki/views.py
@@ -34,27 +34,28 @@ def add_term(request):
 
 def send_term(request):
     """Handles the submission of a new term."""
-    if request.method == "POST":
-        cache.clear()
-        user_name = request.POST.get("name")
-        new_term = request.POST.get("new_term", "")
-        new_definition = request.POST.get("new_definition", "").replace(";", ",")
-        context = {"user": user_name}
-        if len(new_definition) == 0:
-            context["success"] = False
-            context["comment"] = "Описание должно быть не пустым"
-        elif len(new_term) == 0:
-            context["success"] = False
-            context["comment"] = "Термин должен быть не пустым"
-        else:
-            context["success"] = True
-            context["comment"] = "Ваш термин принят"
-            terms_work.write_term(new_term, new_definition)
-        if context["success"]:
-            context["success-title"] = ""
-        return render(request, "terms_request.html", context)
-    else:
+    if request.method != "POST":
         return add_term(request)
+
+    cache.clear()
+    user_name = request.POST.get("name")
+    new_term = request.POST.get("new_term", "")
+    new_definition = request.POST.get("new_definition", "").replace(";", ",")
+    context = {"user": user_name}
+    if len(new_definition) == 0:
+        context["success"] = False
+        context["comment"] = "Описание должно быть не пустым"
+    elif len(new_term) == 0:
+        context["success"] = False
+        context["comment"] = "Термин должен быть не пустым"
+    else:
+        context["success"] = True
+        context["comment"] = "Ваш термин принят"
+        terms_work.write_term(new_term, new_definition)
+    if context["success"]:
+        context["success-title"] = ""
+    return render(request, "terms_request.html", context)
+
 
 def show_stats(request):
     """Renders the stats page."""
